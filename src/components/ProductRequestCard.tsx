7c2612da60fabf4a5324034c8bf90e1004604af4
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2021-11-04 18:06:42 +0100

    finish product-reqquest-card design for mobile

--- src/components/ProductRequestCard.tsx
+++ src/components/ProductRequestCard.tsx
@@ -8,6 +8,30 @@ const ProductRequestCard: React.FC<{ request: any }> = ({ request }) => {
       <span className="font-jost font-semibold text-xs text-blue px-4 py-1.5 bg-gray rounded-large">
         {request.category[0].toUpperCase() + request.category.slice(1)}
       </span>
+      <div className="mt-4 flex justify-between md:invisible">
+        <div className="bg-gray py-2 pl-4 pr-3.5 rounded-large inline-block">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline" viewBox="0 0 20 20" fill="#4661E6">
+            <path
+              fillRule="evenodd"
+              d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span className="font-jost font-bold text-xs text-blue-darkest ml-2">{request.upvotes}</span>
+        </div>
+        <div className="py-2 pl-4 pr-3.5 inline-block">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline" viewBox="0 0 20 20" fill="#F2F4FF">
+            <path
+              fillRule="evenodd"
+              d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <span className="font-jost font-bold text-xs text-blue-darkest ml-2">
+            {request.comments ? request.comments.length : 0}
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Suggestions.tsx
+++ src/pages/Suggestions.tsx
@@ -8,7 +8,6 @@ const Suggestions: React.FC = () => {
   useEffect(() => {
     (async () => {
       const { data } = await axios.get('http://localhost:5001/productRequests');
-      console.log(data);
       setProductRequests(data);
     })();
   }, []);
