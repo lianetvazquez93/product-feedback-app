35f6f5d0c2f9dda44e0a160ce56236ee139c0927
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2021-11-17 17:46:58 +0100

    create navabar-roadmap component

---
+++ src/components/NavbarRoadmap.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavbarRoadmap: React.FC<any> = ({ productRequests }) => {
+  return (
+    <div className="mx-6 mt-5">
+      <div className="flex justify-between flex-none items-center">
+        <span className="font-bold text-lg text-indigo-dark">Roadmap</span>
+        <Link to="/roadmap" className="font-semibold text-sm text-blue underline">
+          View
+        </Link>
+      </div>
+      <div className="flex justify-between flex-none items-center mt-6">
+        <div className="w-32">
+          <div className="h-2 w-2 inline-block" style={{ backgroundColor: '#F49F85', borderRadius: '1rem' }}></div>
+          <span className="font-normal text-base text-gray-dark ml-4">Planned</span>
+        </div>
+        <span className="font-bold text-base text-gray-dark">
+          {productRequests.filter((request: any) => request.status === 'planned').length}
+        </span>
+      </div>
+      <div className="flex justify-between flex-none items-center mt-2">
+        <div className="w-32">
+          <div className="h-2 w-2 inline-block bg-purple" style={{ borderRadius: '1rem' }}></div>
+          <span className="font-normal text-base text-gray-dark ml-4">In-Progress</span>
+        </div>
+        <span className="font-bold text-base text-gray-dark">
+          {productRequests.filter((request: any) => request.status === 'in-progress').length}
+        </span>
+      </div>
+      <div className="flex justify-between flex-none items-center mt-2">
+        <div className="w-32">
+          <div className="h-2 w-2 inline-block" style={{ backgroundColor: '#62BCFA', borderRadius: '1rem' }}></div>
+          <span className="font-normal text-base text-gray-dark ml-4">Live</span>
+        </div>
+        <span className="font-bold text-base text-gray-dark">
+          {productRequests.filter((request: any) => request.status === 'live').length}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarRoadmap;
