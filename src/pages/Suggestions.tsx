1af40355b0ff6f103ff7111db82799865afb4997
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2021-11-19 01:46:56 +0100

    wip: create navbar for mobile screens

--- src/pages/Suggestions.tsx
+++ src/pages/Suggestions.tsx
@@ -149,50 +149,70 @@ const Suggestions: React.FC = () => {
             </button>
           </div>
         </div>
-        <div className="flex justify-between items-center bg-indigo-dark pl-6 pr-3 py-2 mb-8 md:rounded-large md:py-3.5 lg:mt-24">
-          <div className="md:w-3/4 h-7">
-            <div className="invisible w-0 h-0 md:visible md:inline md:w-52 md:h-7">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 inline"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="#FFFFFF"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-                />
-              </svg>
-              <span className="ml-4 text-gray-light font-bold text-lg my-auto">{`${productRequests.length} Suggestions`}</span>
+        <div
+          className={`bg-gray-light absolute bottom-0 top-20 right-0 md:w-0 md:invisible ${
+            navbarVisible ? 'w-72' : 'w-0 invisible'
+          }`}
+        >
+          <div className="mx-6 mt-6 flex flex-col">
+            <div className="w-full bg-white rounded-large h-44">
+              <NavbarFilters selectFilter={setFilterType} filterTypes={filterTypes} filterType={filterType} />
             </div>
-            <div className="md:inline md:ml-10">
-              <label htmlFor="sort" className="text-sm text-gray-light">
-                Sort by:
-              </label>
-              <select
-                id="sort"
-                className="border-0 ml-1 bg-transparent text-sm text-gray-light font-bold focus:border-0"
-                value={sortType}
-                onChange={(e) => setSortType(e.target.value)}
-              >
-                <option value="Most Upvotes">Most Upvotes</option>
-                <option value="Least Upvotes">Least Upvotes</option>
-                <option value="Most Comments">Most Comments</option>
-                <option value="Least Comments">Least Comments</option>
-              </select>
+            <div className="w-full bg-white rounded-large mt-6 h-44">
+              <NavbarRoadmap productRequests={productRequests} />
             </div>
           </div>
-          <Link
-            to="/new"
-            className="inline-block align-middle bg-purple text-sm text-gray-light font-bold rounded-large hover:bg-purple-light py-2.5 px-4 md:py-3"
-          >
-            + Add Feedback
-          </Link>
         </div>
-        {productRequests.length > 0 ? <ProductRequestsList productRequests={productRequests} /> : <NoProductRequests />}
+        <div>
+          <div className="flex justify-between items-center bg-indigo-dark pl-6 pr-3 py-2 mb-8 md:rounded-large md:py-3.5 lg:mt-24">
+            <div className="md:w-3/4 h-7">
+              <div className="invisible w-0 h-0 md:visible md:inline md:w-52 md:h-7">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 inline"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="#FFFFFF"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                  />
+                </svg>
+                <span className="ml-4 text-gray-light font-bold text-lg my-auto">{`${productRequests.length} Suggestions`}</span>
+              </div>
+              <div className="md:inline md:ml-10">
+                <label htmlFor="sort" className="text-sm text-gray-light">
+                  Sort by:
+                </label>
+                <select
+                  id="sort"
+                  className="border-0 ml-1 bg-transparent text-sm text-gray-light font-bold focus:border-0"
+                  value={sortType}
+                  onChange={(e) => setSortType(e.target.value)}
+                >
+                  <option value="Most Upvotes">Most Upvotes</option>
+                  <option value="Least Upvotes">Least Upvotes</option>
+                  <option value="Most Comments">Most Comments</option>
+                  <option value="Least Comments">Least Comments</option>
+                </select>
+              </div>
+            </div>
+            <Link
+              to="/new"
+              className="inline-block align-middle bg-purple text-sm text-gray-light font-bold rounded-large hover:bg-purple-light py-2.5 px-4 md:py-3"
+            >
+              + Add Feedback
+            </Link>
+          </div>
+          {productRequests.length > 0 ? (
+            <ProductRequestsList productRequests={productRequests} />
+          ) : (
+            <NoProductRequests />
+          )}
+        </div>
       </div>
     </div>
   );
