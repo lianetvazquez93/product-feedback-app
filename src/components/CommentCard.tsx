5ee0280ccdc1e6e1a9660816b3367d6c272ea8b3
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2021-11-29 18:51:31 +0100

    add content to comment-card

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Comment } from '../pages/Suggestions';
+import imageelijah from '../assets/user-images/image-elijah.jpg';
 
 interface CommentCardProps {
   comment: Comment;
@@ -10,7 +11,7 @@ const CommentCard: React.FC<CommentCardProps> = ({ comment }) => {
     <div className="mb-6">
       <div className="flex justify-between h-10 items-center">
         <div className="flex justify-start items-start h-full">
-          <img src={comment.user.image} className="h-full w-10 rounded-full" />
+          <img src={imageelijah} className="h-full w-10 rounded-full" />
           <div className="h-full ml-4 md:ml-8 w-32">
             <p className="font-bold text-sm text-indigo">{comment.user.name}</p>
             <p className="text-sm text-gray-dark">@{comment.user.username}</p>
@@ -18,6 +19,7 @@ const CommentCard: React.FC<CommentCardProps> = ({ comment }) => {
         </div>
         <button className="font-semibold text-sm text-blue hover:underline p-0">Reply</button>
       </div>
+      <p className="mt-4 text-sm md:text-base text-gray-dark mx-0 md:ml-16">{comment.content}</p>
     </div>
   );
 };
